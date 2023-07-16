a3d43c5b201014ce3b1d8f6a5386cb782f8dd504
feDann <daniele99@ferrazzo.it>
2023-07-16 23:38:41 +0200

    Add correlator interface

--- src/correlator.cu
+++ src/correlator.cu
@@ -1,1 +1,41 @@
 #include "correlator.cuh"
+#include <cassert>
+
+template <typename T>
+__global__ void MultiTau::correlate<T>(T * new_values, size_t packet_size, T * shift_register, T * accumulator, T * insert_indexes, T * correlation){
+
+};
+
+template <typename T>
+Correlator<T>::Correlator(size_t _num_bins, size_t _bin_size, size_t _num_sensors, bool _verbose){    
+    num_bins = _num_bins;
+    bin_size = _bin_size;
+    num_sensors = _num_sensors;
+    verbose = _verbose;    
+};
+
+template <typename T>
+Correlator<T>::~Correlator(){
+    free(correlation);
+    free(taus);
+};
+
+template <typename T>
+void Correlator<T>::alloc(){
+
+};
+
+template <typename T>
+void Correlator<T>::correlate(T * new_values, size_t packet_size){
+
+};
+
+template <typename T>
+void Correlator<T>::outputs(T * correlations, uint32_t* t){
+
+};
+
+template <typename T>
+void Correlator<T>::reset(){
+
+};
--- src/correlator.cuh
+++ src/correlator.cuh
@@ -1,1 +1,42 @@
 #pragma once
+
+#include <cuda_runtime.h>
+#include <iostream>
+namespace MultiTau {
+
+    template <typename T>
+    __global__ void correlate (T * new_values, size_t packet_size, T * shift_register, T * accumulator, T * insert_indexes, T * correlation);
+
+}
+
+template<typename T>
+class Correlator {
+
+    public:
+        Correlator(size_t num_bins, size_t bin_size, size_t num_sensors, bool verbose = false);
+        ~Correlator();
+
+        void alloc();
+        void correlate(T * new_values, size_t packet_size);
+        void outputs(T * correlations, uint32_t* t);
+        void reset();
+
+    private:
+        size_t num_bins;
+        size_t bin_size;
+        size_t num_sensors;
+
+        bool verbose;
+
+        T * correlation;
+        uint32_t * taus;
+
+        // device variables
+        T * d_shift_register;
+        T * d_accumulator;
+        T * d_insert_indexes;
+        T * d_correlation;
+        T * d_new_values;
+
+
+};
