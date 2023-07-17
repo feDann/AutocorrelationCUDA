3f965ae739cc9a8e40e9057876cf07d99aeaf43e
feDann <daniele99@ferrazzo.it>
2023-07-17 19:57:41 +0200

    Add basic main

--- src/main.cu
+++ src/main.cu
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "utils.hpp"
+#include "options.hpp"
 #include "correlator.cuh"
 
 
 int main (int argc, char* argv[]){
+    Options options(argc, argv);
+    Correlator<int32_t> correlator(options.num_bins, options.bin_size, options.num_sensors, 8, 0, options.debug);
 
     std::cout << "Hello, World!" << std::endl ;
 
