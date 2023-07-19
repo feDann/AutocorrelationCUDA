91eb0a1403af594502037f780eec0aa67e9bc438
feDann <daniele99@ferrazzo.it>
2023-07-19 20:40:01 +0200

    Update run

--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,9 @@
 
 NVIDIA_SMI_ARGS="-lms 1 --format=csv --query-gpu=timestamp,power.draw,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used"
 AUTOCORR_ARGS="-I 30 -r"
-OUTPUT_FOLDER="output/g32-l10"
+OUTPUT_FOLDER="output/g32-l16"
+
+make
 
 if [ ! -d "$OUTPUT_FOLDER" ]; then
     mkdir -p "$OUTPUT_FOLDER"
@@ -24,7 +26,7 @@ sleep 1
 echo "Running for 100.csv"
 
 nvidia-smi $NVIDIA_SMI_ARGS -f "${OUTPUT_FOLDER}/100-utilization.csv" & pid4=$!
-./main -p 10 -i ../input/100.csv  -o "${OUTPUT_FOLDER}/100.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/100" & pid3=$!
+./main -p 100 -i ../input/100.csv  -o "${OUTPUT_FOLDER}/100.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/100" & pid3=$!
 
 wait $pid3
 sleep 1
@@ -35,7 +37,7 @@ sleep 1
 echo "Running for 1000.csv"
 
 nvidia-smi $NVIDIA_SMI_ARGS -f "${OUTPUT_FOLDER}/1000-utilization.csv" & pid6=$!
-./main -p 100 -i ../input/1000.csv  -o "${OUTPUT_FOLDER}/1000.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/1000" & pid5=$!
+./main -p 1000 -i ../input/1000.csv  -o "${OUTPUT_FOLDER}/1000.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/1000" & pid5=$!
 
 wait $pid5
 sleep 1
@@ -46,7 +48,7 @@ sleep 1
 echo "Running for 10000.csv"
 
 nvidia-smi $NVIDIA_SMI_ARGS -f "${OUTPUT_FOLDER}/10000-utilization.csv" & pid8=$!
-./main -p 1000 -i ../input/10000.csv  -o "${OUTPUT_FOLDER}/10000.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/10000" & pid7=$!
+./main -p 10000 -i ../input/10000.csv  -o "${OUTPUT_FOLDER}/10000.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/10000" & pid7=$!
 
 wait $pid7
 sleep 1
@@ -57,7 +59,7 @@ sleep 1
 echo "Running for 30000.csv"
 
 nvidia-smi $NVIDIA_SMI_ARGS -f "${OUTPUT_FOLDER}/30000-utilization.csv" & pid10=$!
-./main -p 1000 -i ../input/30000.csv  -o "${OUTPUT_FOLDER}/30000.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/30000" & pid9=$!
+./main -p 30000 -i ../input/30000.csv  -o "${OUTPUT_FOLDER}/30000.csv" $AUTOCORR_ARGS > "${OUTPUT_FOLDER}/30000" & pid9=$!
 
 wait $pid9
 sleep 1
