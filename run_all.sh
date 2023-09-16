8313e682c9918426a95d10422aa49562a1ad38b2
feDann <daniele99@ferrazzo.it>
2023-09-16 19:21:28 +0200

    Update run configuration

--- run_all.sh
+++ run_all.sh
@@ -2,15 +2,22 @@
 
 set -e
 
-TIMEPOINTS="100 1000 2048 10000 30000"
+TIMEPOINTS="10 100 1000 2048 10000 30000"
 
 NVIDIA_SMI_ARGS="-lms 1 --format=csv --query-gpu=timestamp,power.draw,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used"
 OUTPUT_FOLDER="output"
-INPUT_FOLDER="../thesis/input"
+INPUT_FOLDER="../input"
 
 
 LEVELS_CONFIG="8 10 16"
 GROUP_SIZE_CONFIG="8 16 32"
+
+
+echo "Compiling..."
+
+make
+
+echo "-----------------------------------"
 
 
 for TIMEPOINT in $TIMEPOINTS; do
